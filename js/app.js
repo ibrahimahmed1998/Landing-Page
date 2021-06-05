0751fa2933c389f920d3c86253377cd4fad4d04e
ibrahim ahmed <hema.1998.man@gmail.com>
2021-06-05 17:01:47 +0200

    t

--- js/app.js
+++ js/app.js
@@ -1,32 +1,57 @@
-const x = document.querySelectorAll("section");
+var x = document.querySelectorAll("section");
+var ul=document.getElementById("navbar__list");
 
 function helpers(x)
  {
-      var ul=document.getElementById("navbar__list");
-
       for (var i = 0; i < x.length; i++)
       {   
         text = document.createTextNode(x[i].id);
-        var span = document.createElement("span");
-        
         var anc = document.createElement("a");
         anc.appendChild(text);
-        span.appendChild(anc);
-        // <a herf=`#${seclink}` > ${secname} </a>`';
-
         anc.setAttribute('href',`#${x[i].id}`);
+        anc.setAttribute('name',`${x[i].id}`);
         anc.setAttribute('class','menu__link');
-
+ 
         var li = document.createElement("li");
-        li.appendChild(span);
-        li.setAttribute('class','abc');
+        li.appendChild(anc);
+        li.setAttribute('id','li');
         ul.appendChild(li);
+
        }
-       console.log(ul);
 }
 
 helpers(x);
+
+document.addEventListener("click",function()
+{
+  var xx = event.clientX, y = event.clientY,
+  elementMouseIsOver = document.elementFromPoint(xx, y);
+  var in_ = elementMouseIsOver.innerHTML ; 
+  
+  var e = document.getElementById(in_);
+ // console.log(e);
+
+
+  for (let index = 0; index < x.length; index++) 
+  {
+    x[index].removeAttribute('class');
+  }
+
+  e.setAttribute('class', 'your-active-class');
+
+  var element = document.getElementById('test98'); 
+  element.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+  
+});
+
+
+  
  
 
 
  
+
+
+ 
+
+
